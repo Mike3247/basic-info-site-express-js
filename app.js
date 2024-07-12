a58c27e2cd829a013222e2aa0b08bf7dbaf7bcff
Mike3247 <72448458+Mike3247@users.noreply.github.com>
2024-07-12 15:22:11 -0400

    add 404 response route

--- app.js
+++ app.js
@@ -24,3 +24,12 @@ app.get('/contact', (req, res) => {
     res.sendFile('./contact.html', { root: __dirname });
 });
 
+// redirects
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+});
+
+// 404 page
+app.use((req, res) => {
+    res.status(404).sendFile('./404.html', { root: __dirname });
+});
